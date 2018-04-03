22c128905b11ac3fd31183f81e7d4431b2a0542c
liuyuqian <liuyuqian@sensetime.com>
2018-04-03 22:09:37 +0800

    shutdown GBA(LD)

--- run_KITTI_stereo.sh
+++ run_KITTI_stereo.sh
@@ -1,2 +1,2 @@
-./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI00-02.yaml /data/KITTI/dataset/sequences/01/
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI00-02.yaml /data/KITTI/dataset/sequences/05/ ./
 
