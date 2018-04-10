954c9caace67e628adb0a49e7e470bd67ba59008
liuyuqian <liuyuqian@sensetime.com>
2018-04-10 16:37:18 +0800

    half K

--- run_KITTI_stereo.sh
+++ run_KITTI_stereo.sh
@@ -1,2 +1,2 @@
-./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI00-02.yaml /data/KITTI/dataset/sequences/05/ ./
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI00-02.yaml /home/user/Data/Kitti/dataset/sequences/05/ ./
 
